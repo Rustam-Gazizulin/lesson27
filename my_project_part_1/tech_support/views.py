6d83c9945bfa632fb4fcb03d4d4dbf5ae9a2cd6e
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-06 23:46:27 +0500

    init

--- my_project_part_1/tech_support/views.py
+++ my_project_part_1/tech_support/views.py
@@ -5,6 +5,20 @@ from tech_support.models import Statistic
 
 
 def statistics(request):
-    # TODO напишите view-функцию которая возвращает всю статистику
-    #  обращений в тех-поддержку (задание tech_support)
-    pass
+    support_calls = Statistic.objects.all()
+    response = []
+    for statistic in support_calls:
+        response.append({
+            "id": statistic.id,
+            "author": statistic.author,
+            "day": statistic.day,
+            "store": statistic.store,
+            "reason": statistic.reason,
+            "status": statistic.status,
+            "timestamp": statistic.timestamp,
+        })
+    return JsonResponse(response, safe=False)
+
+
+
+
