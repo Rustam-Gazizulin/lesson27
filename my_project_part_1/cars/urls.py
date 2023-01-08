600bdc1fda546df4c8828cc00ae0d75b52b64cf7
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-08 12:16:38 +0500

    cars

--- my_project_part_1/cars/urls.py
+++ my_project_part_1/cars/urls.py
@@ -1,2 +1,8 @@
-# TODO настройте здесь urls для заданий get_car и search_car)
-urlpatterns = []
+from django.urls import path
+from cars import views
+
+urlpatterns = [
+    path('', views.list_car, name='list_cars'),
+    path('<int:pk>/', views.get_car, name='car'),
+    path('search/', views.search, name='search_brand'),
+]
--- my_project_part_1/cars/views.py
+++ my_project_part_1/cars/views.py
@@ -1,8 +1,53 @@
+from cars.models import Car
+from django.http import JsonResponse
+
+def list_car(request):
+    cars = Car.objects.all()
+    response = []
+    for car in cars:
+        response.append({
+            'id': car.id,
+            'slug': car.slug,
+            'name': car.name,
+            'brand': car.brand,
+            'address': car.address,
+            'description': car.description,
+            'status': car.status,
+            'created': car.created,
+        })
+    return JsonResponse(response, safe=False)
+
 def get_car(request, pk):
-    # TODO напишите view-функцию здесь (Readme.md, Задание get_car)
-    pass
+    try:
+        cars = Car.objects.get(id=pk)
+    except Car.DoesNotExist:
+        return JsonResponse({'error': 'not found'}, status=404)
+
+    return JsonResponse({
+        'id': cars.id,
+        'slug': cars.slug,
+        'name': cars.name,
+        'brand': cars.brand,
+        'address': cars.address,
+        'description': cars.description,
+        'status': cars.status,
+        'created': cars.created,
+    })
 
 
 def search(request):
-    # TODO напишите view-функцию здесь (Readme.md, Задание car_search)
-    pass
+    cars = Car.objects.all()
+    search_brand = request.GET.get('brand', None)
+    if search_brand:
+        cars = cars.filter(brand=search_brand)
+
+    response = []
+    for car in cars:
+        response.append({
+            'id': car.id,
+            'name': car.name,
+            'brand': car.brand,
+            'status': car.status,
+        })
+    return JsonResponse(response, safe=False)
+
